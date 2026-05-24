b192a7f4f880cd181f5f7a1086d78d9fc7286dc2
Victor Berlinck <193505785+Victornck@users.noreply.github.com>
2026-05-23 21:43:58 -0300

    telas e icones criados e testados

---
+++ app/src/main/java/com/example/estagioapp/PerfilActivity.java
@@ -0,0 +1,14 @@
+package com.example.estagioapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil);
+    }
+}
